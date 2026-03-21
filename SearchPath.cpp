c750ee3282005620678e1ef45ff3749d74307fe7
Thomas Paul Betterly <thomasbetterly@aol.com>
2026-03-21 17:27:59 -0400

    .

--- SearchPath.cpp
+++ SearchPath.cpp
@@ -4,18 +4,36 @@
 extern "C" {
 
 	HMODULE Kernel32 = 0;
-
+#warning SearchPathsForTarget needs check and balances to ensure no acciently buffer overflow
 	bool SearchPathsForTarget(const char* filename, LWAnsiString* OutputIfOk, LWAnsiString* OutputIfNotFound)
 	{
+		DWORD already_happened = 0;
+		again:
 		SetLastError(0);
 		DWORD result = SearchPathA(NULL, filename, NULL, OutputIfOk->AllocatedSize, OutputIfOk->Data, NULL);
-		if (result != 0)
+
+		if ((result != 0) &&  (result < OutputIfOk->AllocatedSize))
 		{
 			LWAnsiString_ProbeLength(OutputIfOk);
 			return true;
 		}
 		else
 		{
+			if (OutputIfOk->AllocatedSize < result)
+			{
+				LWAnsiString_AddReserve(OutputIfOk, result );
+				if (!already_happened)
+				{
+					already_happened = true;
+					goto again;
+				}
+				else
+				{
+					return false;
+				}
+
+			}
+
 			if (GetLastError() == ERROR_FILE_NOT_FOUND)
 			{
 				LWAnsiString_Append(OutputIfNotFound, "[NOT FOUND] ");
@@ -23,6 +41,8 @@ extern "C" {
 			}
 			else
 			{
+				
+
 				LWAnsiString_Append(OutputIfNotFound, "[NOT FOUND] ");
 				LWAnsiString_Append(OutputIfNotFound, filename);
 				LWAnsiString_Append(OutputIfNotFound, "->last error for search is->");
